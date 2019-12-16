656f8d75c75dec1ada0fd444735a616b1d786c8d
dayvid-lopes <dayvidlopes98@gmail.com>
2019-12-15 22:28:24 -0300

    Questão 884 finalizada

--- Lista 1/884 - Japanese Crosswords Strike Back.c
+++ Lista 1/884 - Japanese Crosswords Strike Back.c
@@ -4,7 +4,21 @@ FILE *saida;
 
 int main(){
     saida = fopen("Lista 1/Outputs/Output 884", "w");
-    
+    int n, x;
+    scanf("%d %d", &n, &x);
+    int i;
+    for(i = 0; i < n; i++){
+        int a;
+        scanf("%d", &a);
+        x -= a;
+        x--;
+    }
+    if(x == -1 || n == 0){
+        imprima("YES");
+    }
+    else{
+        imprima("NO");
+    }
     fflush(saida);
     free(saida);
     return 0;
